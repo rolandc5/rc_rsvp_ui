c6ab50e25eba18fbb56e3951c6d3c6503cce5f76
Roland Canuto <roland@Rolands-MacBook-Pro.local>
2025-01-23 22:33:27 -0800

    Added Headers for rsvp page and initial commit

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'r-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+
+}
