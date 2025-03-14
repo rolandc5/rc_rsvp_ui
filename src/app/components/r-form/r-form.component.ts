502c2d84ace4a6e53067820701a6ef0b550bffa3
Roland Canuto <rolandcanuto@outlook.com>
2025-03-14 00:24:30 -0700

    added animations to form pages

--- src/app/components/r-form/r-form.component.ts
+++ src/app/components/r-form/r-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SheetsService } from '../../services/sheets.service';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 interface Rsvp {
   range: [number],
@@ -12,7 +13,20 @@ interface Rsvp {
   standalone: true,
   imports: [ReactiveFormsModule, FormsModule],
   templateUrl: './r-form.component.html',
-  styleUrl: './r-form.component.scss'
+  styleUrl: './r-form.component.scss',
+  animations: [
+    trigger('slideInOut', [
+      // This is for the "enter" animation, if you want it to fade in when it enters
+      transition(':enter', [
+        style({ opacity: 0, transform: "translateX(60px)" }),
+        animate('500ms', style({ opacity: 1, transform: "translateX(0px)" }))
+      ]),
+      // This is for the "leave" animation, it fades out when removed
+      transition(':leave', [
+        animate('500ms', style({ opacity: 0, transform: "translateX(-60px)" }))
+      ])
+    ])
+  ]
 })
 export class RFormComponent implements OnInit {
   sheetService = inject(SheetsService);
@@ -23,8 +37,10 @@ export class RFormComponent implements OnInit {
     2: 'invitation',
     3: 'allergy',
     4: 'plus1',
-    5: 'songRequest'
+    5: 'songRequest',
+    6: 'wow'
   }
+  pageCache: number = 1;
   page: string = this.pageEnum[1];
 
 
@@ -32,10 +48,31 @@ export class RFormComponent implements OnInit {
 
   }
 
+  onLeaveAnimationDone(e: any) {
+    if (e.toState === 'void') {
+      if (this.pageCache === 1) {
+        this.page = this.pageEnum[2];
+        this.pageCache = 2;
+      }
+      else if (this.pageCache === 2) {
+        this.page = this.pageEnum[3];
+        this.pageCache = 3;
+      }
+      else if (this.pageCache === 3) {
+        this.page = this.pageEnum[4];
+        this.pageCache = 4;
+      }
+      else if (this.pageCache === 4) {
+        this.page = this.pageEnum[5];
+        this.pageCache = 5;
+      }
+    }
+  }
+
   searchInviteInfo() {
     this.sheetService.getInviteInfo(this.name).subscribe((data: any) => {
       this.rsvpLists = data;
-      this.page = this.pageEnum[2];
+      this.page = this.pageEnum[6];
     }, err => {
       console.log(err);
     });
@@ -56,16 +93,15 @@ export class RFormComponent implements OnInit {
         invitee[0] = 'no';
       }
     });
-    this.page = this.pageEnum[3];
+    this.page = this.pageEnum[6];
   }
 
   allergySubmit() {
-    
-    this.page = this.pageEnum[4];
+    this.page = this.pageEnum[6];
   }
 
   plusOneSubmit() {
-    this.page = this.pageEnum[5];
+    this.page = this.pageEnum[6];
   }
 
   onSubmit() {
