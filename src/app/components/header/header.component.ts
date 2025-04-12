27cf20d6c57f45820b8da129521018f72f09acf2
Roland Canuto <rolandcanuto@outlook.com>
2025-04-12 12:10:41 -0700

    updates navigation to full page

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { PackagesModule } from '../../modules/packages.module';
 
 @Component({
   selector: 'r-header',
   standalone: true,
-  imports: [],
+  imports: [PackagesModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-
+  navMenuPopup: boolean = false;
 }
